15b0c8f0d02555d117a502afc7b16c8740379e78
evtepo <andrey_laguta00@mail.ru>
2024-10-25 23:05:04 +1000

    добавить модели product и sales

---
+++ sales_service/models/product.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+from uuid import UUID
+
+from sqlalchemy import DateTime, Numeric, ForeignKey, String, Text
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from db.db_connect import Base
+from models.base import TableMixin
+from models.location import Store
+
+
+class Sales(Base, TableMixin):
+    store_id: Mapped[UUID] = mapped_column(ForeignKey("store.id", ondelete="CASCADE"), nullable=False)
+    city_id: Mapped[UUID] = mapped_column(ForeignKey("city.id", ondelete="CASCADE"), nullable=False)
+    amount: Mapped[float] = mapped_column(Numeric(precision=9, scale=2), nullable=False)
+    sale_date: Mapped[datetime] = mapped_column(DateTime, default=datetime.now, nullable=False)
+
+    products: Mapped["Product"] = relationship(back_populates="sales")
+
+
+class Product(Base, TableMixin):
+    name: Mapped[str] = mapped_column(String(255), nullable=False)
+    description: Mapped[str] = mapped_column(Text)
+    price: Mapped[float] = mapped_column(Numeric(precision=9, scale=2), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        default=datetime.now,
+        nullable=False,
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        default=datetime.now,
+        onupdate=datetime.now,
+        nullable=False,
+    )
+    store_id: Mapped[UUID] = mapped_column(ForeignKey("store.id", ondelete="CASCADE"), nullable=False)
+    sales_id: Mapped[UUID | None] = mapped_column(ForeignKey("sales.id", ondelete="CASCADE"))
+
+    sales: Mapped["Sales"] = relationship(back_populates="products")
+    store: Mapped["Store"] = relationship(back_populates="products")
