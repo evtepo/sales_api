3ba609cbdb159e1036e5f3bb7889c4cce332586d
evtepo <andrey_laguta00@mail.ru>
2024-10-27 01:09:45 +1000

    изменить логику взаимодействия для city

--- sales_service/api/v1/city.py
+++ sales_service/api/v1/city.py
@@ -1,30 +1,37 @@
+from typing import Annotated
 from uuid import UUID
 
-from fastapi import APIRouter, Query, status
+from fastapi import APIRouter, Depends, Query, status
 
 from schemas.response import CityResponse, SingleCityResponse
 from schemas.city import CreateCity, DeleteCity, UpdateCity
-from services.city import (
-    delete_city_by_id,
-    get_list_of_cities,
-    get_single_city,
-    new_city,
-    update_city_by_id
-)
+from services.city import CityService, get_city_service
 from utils.dependency import db_dependency, repository_dependency
 
 
 router = APIRouter(prefix="/api/v1/city", tags=["City"])
 
+city_dependency = Annotated[CityService, Depends(get_city_service)]
+
 
 @router.post("/", response_model=CityResponse, status_code=status.HTTP_201_CREATED)
-async def create_city(city: CreateCity, repository: repository_dependency, session: db_dependency):
-    return await new_city(city, repository, session)
+async def create_city(
+    city: CreateCity,
+    repository: repository_dependency,
+    session: db_dependency,
+    city_service: city_dependency,
+):
+    return await city_service.new_city(city, repository, session)
 
 
 @router.get("/{city_id}", response_model=SingleCityResponse, status_code=status.HTTP_200_OK)
-async def get_city(city_id: UUID, repository: repository_dependency, session: db_dependency):
-    return await get_single_city(city_id, repository, session)
+async def get_city(
+    city_id: UUID,
+    repository: repository_dependency,
+    session: db_dependency,
+    city_service: city_dependency,
+):
+    return await city_service.get_single_city(city_id, repository, session)
 
 
 @router.get(
@@ -35,17 +42,28 @@ async def get_city(city_id: UUID, repository: repository_dependency, session: db
 async def get_cities(
     repository: repository_dependency,
     session: db_dependency,
+    city_service: city_dependency,
     page: int = Query(ge=1, default=1),
     size: int = Query(ge=10, le=50, default=10),
 ):
-    return await get_list_of_cities(page, size, repository, session)
+    return await city_service.get_list_of_cities(page, size, repository, session)
 
 
 @router.put("/", response_model=CityResponse, status_code=status.HTTP_200_OK)
-async def update_city(city: UpdateCity, repository: repository_dependency, session: db_dependency):
-    return await update_city_by_id(city, repository, session)
+async def update_city(
+    city: UpdateCity,
+    repository: repository_dependency,
+    session: db_dependency,
+    city_service: city_dependency,
+):
+    return await city_service.update_city_by_id(city, repository, session)
 
 
 @router.delete("/", response_model=dict[str, str], status_code=status.HTTP_200_OK)
-async def delete_city(city: DeleteCity, repository: repository_dependency, session: db_dependency):
-    return await delete_city_by_id(city, repository, session)
+async def delete_city(
+    city: DeleteCity,
+    repository: repository_dependency,
+    session: db_dependency,
+    city_service: city_dependency,
+):
+    return await city_service.delete_city_by_id(city, repository, session)
--- sales_service/services/city.py
+++ sales_service/services/city.py
@@ -5,54 +5,29 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from models.location import City
 from repository.repository import BaseRepository
 from schemas.city import CreateCity, DeleteCity, UpdateCity
-from utils.error_handling import error_response
+from services.base import BaseService
 
 
-async def new_city(data: CreateCity, repository: BaseRepository, session: AsyncSession):
-    data = data.model_dump()
-    return await repository.create(data, City, session)
+class CityService(BaseService):
+    async def new_city(self, data: CreateCity, repository: BaseRepository, session: AsyncSession):
+        return await self.create_new_row(data, City, repository, session)
+
+    async def get_single_city(self, city_id: UUID, repository: BaseRepository, session: AsyncSession):
+        related_fields = ("stores",)
+        return await self.get_single_row(city_id, related_fields, City, repository, session)
+
+    async def get_list_of_cities(self, offset: int, limit: int, repository: BaseRepository, session: AsyncSession):
+        return await self.get_list_rows(offset, limit, City, repository, session)
+
+    async def update_city_by_id(self, data: UpdateCity, repository: BaseRepository, session: AsyncSession):
+        return await self.update_row_by_id(data, City, repository, session)
+
+    async def delete_city_by_id(self, data: DeleteCity, repository: BaseRepository, session: AsyncSession):
+        return await self.delete_row_by_id(data, City, repository, session)
 
 
-async def get_single_city(city_id: UUID, repository: BaseRepository, session: AsyncSession):
-    filters = {"id": city_id}
-    related_fields = ("stores",)
-    result = await repository.get_single(City, session, related_fields, **filters)
-    if not result:
-        return error_response("City with such ID not found.")
-
-    return result
+city_service = CityService()
 
 
-async def get_list_of_cities(offset: int, limit: int, repository: BaseRepository, session: AsyncSession):
-    offset_arg = (offset - 1) * limit
-    result = await repository.get_list(City, session, limit, offset_arg)
-
-    prev_page = offset - 1 if offset > 1 else None
-    next_page = offset + 1 if len(result) == limit else None
-
-    return {
-        "links": {
-            "prev": prev_page,
-            "next": next_page,
-        },
-        "data": result,
-    }
-
-
-async def update_city_by_id(data: UpdateCity, repository: BaseRepository, session: AsyncSession):
-    data = data.model_dump()
-    filters = {"id": data.get("id")}
-    result = await repository.update(data, City, session, **filters)
-    if not result:
-        return error_response("Can't update a city with this ID.")
-
-    return result
-
-
-async def delete_city_by_id(data: DeleteCity, repository: BaseRepository, session: AsyncSession):
-    filters = data.model_dump()
-    result = await repository.delete(City, session, **filters)
-    if not result:
-        return error_response("Can't delete a city with this ID.")
-
-    return result
+async def get_city_service():
+    return city_service
