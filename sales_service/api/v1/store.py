4bb09c71e75f06de23b2de5a63d68f6159c518f9
evtepo <andrey_laguta00@mail.ru>
2024-10-27 01:10:01 +1000

    изменить логику взаимодействия для store

--- sales_service/api/v1/store.py
+++ sales_service/api/v1/store.py
@@ -1,30 +1,37 @@
+from typing import Annotated
 from uuid import UUID
 
-from fastapi import APIRouter, Query, status
+from fastapi import APIRouter, Depends, Query, status
 
 from schemas.response import SingleStoreResponse, StoreResponse
 from schemas.store import CreateStore, DeleteStore, UpdateStore
-from services.store import (
-    delete_store_by_id,
-    get_list_of_stores,
-    get_single_store,
-    new_store,
-    update_store_by_id
-)
+from services.store import get_store_service, StoreService
 from utils.dependency import db_dependency, repository_dependency
 
 
 router = APIRouter(prefix="/api/v1/store", tags=["Store"])
 
+store_dependency = Annotated[StoreService, Depends(get_store_service)]
+
 
 @router.post("/", response_model=StoreResponse, status_code=status.HTTP_201_CREATED)
-async def create_store(store: CreateStore, repository: repository_dependency, session: db_dependency):
-    return await new_store(store, repository, session)
+async def create_store(
+    store: CreateStore,
+    repository: repository_dependency,
+    session: db_dependency,
+    store_service: store_dependency,
+):
+    return await store_service.new_store(store, repository, session)
 
 
 @router.get("/{store_id}", response_model=SingleStoreResponse, status_code=status.HTTP_200_OK)
-async def get_store(store_id: UUID, repository: repository_dependency, session: db_dependency):
-    return await get_single_store(store_id, repository, session)
+async def get_store(
+    store_id: UUID,
+    repository: repository_dependency,
+    session: db_dependency,
+    store_service: store_dependency,
+):
+    return await store_service.get_single_store(store_id, repository, session)
 
 
 @router.get(
@@ -35,17 +42,28 @@ async def get_store(store_id: UUID, repository: repository_dependency, session: 
 async def get_stores(
     repository: repository_dependency,
     session: db_dependency,
+    store_service: store_dependency,
     page: int = Query(ge=1, default=1),
     size: int = Query(ge=10, le=50, default=10),
 ):
-    return await get_list_of_stores(size, page, repository, session)
+    return await store_service.get_list_of_stores(size, page, repository, session)
 
 
 @router.put("/", response_model=StoreResponse, status_code=status.HTTP_200_OK)
-async def update_store(store: UpdateStore, repository: repository_dependency, session: db_dependency):
-    return await update_store_by_id(store, repository, session)
+async def update_store(
+    store: UpdateStore,
+    repository: repository_dependency,
+    session: db_dependency,
+    store_service: store_dependency,
+):
+    return await store_service.update_store_by_id(store, repository, session)
 
 
 @router.delete("/", response_model=dict[str, str], status_code=status.HTTP_200_OK)
-async def delete_store(store: DeleteStore, repository: repository_dependency, session: db_dependency):
-    return await delete_store_by_id(store, repository, session)
+async def delete_store(
+    store: DeleteStore,
+    repository: repository_dependency,
+    session: db_dependency,
+    store_service: store_dependency,
+):
+    return await store_service.delete_store_by_id(store, repository, session)
--- sales_service/services/store.py
+++ sales_service/services/store.py
@@ -5,54 +5,29 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from models.location import Store
 from repository.repository import BaseRepository
 from schemas.store import CreateStore, DeleteStore, UpdateStore
-from utils.error_handling import error_response
+from services.base import BaseService
 
 
-async def new_store(store: CreateStore, repository: BaseRepository, session: AsyncSession):
-    data = store.model_dump()
-    return await repository.create(data, Store, session)
+class StoreService(BaseService):
+    async def new_store(self, store: CreateStore, repository: BaseRepository, session: AsyncSession):
+        return await self.create_new_row(store, Store, repository, session)
+
+    async def get_single_store(self, store_id: UUID, repository: BaseRepository, session: AsyncSession):
+        related_fields = ("city", "products")
+        return await self.get_single_row(store_id, related_fields, Store, repository, session)
+
+    async def get_list_of_stores(self, limit: int, offset: int, repository: BaseRepository, session: AsyncSession):
+        return await self.get_list_rows(offset, limit, Store, repository, session)
+
+    async def update_store_by_id(self, store: UpdateStore, repository: BaseRepository, session: AsyncSession):
+        return await self.update_row_by_id(store, Store, repository, session)
+
+    async def delete_store_by_id(self, store: DeleteStore, repository: BaseRepository, session: AsyncSession):
+        return await self.delete_row_by_id(store, Store, repository, session)
 
 
-async def get_single_store(store_id: UUID, repository: BaseRepository, session: AsyncSession):
-    filters = {"id": store_id}
-    related_fields = ("city", "products")
-    result = await repository.get_single(Store, session, related_fields, **filters)
-    if not result:
-        return error_response("Store with such ID not found.")
-
-    return result
+store_service = StoreService()
 
 
-async def get_list_of_stores(limit: int, offset: int, repository: BaseRepository, session: AsyncSession):
-    offset_arg = (offset - 1) * limit
-    result = await repository.get_list(Store, session, limit, offset_arg)
-
-    prev_page = offset - 1 if offset > 1 else None
-    next_page = offset + 1 if len(result) == limit else None
-
-    return {
-        "links": {
-            "prev": prev_page,
-            "next": next_page,
-        },
-        "data": result,
-    }
-
-
-async def update_store_by_id(store: UpdateStore, repository: BaseRepository, session: AsyncSession):
-    data = store.model_dump()
-    filters = {"id": data.get("id")}
-    result = await repository.update(data, Store, session, **filters)
-    if not result:
-        return error_response("Can't update a store with this ID.")
-
-    return result
-
-
-async def delete_store_by_id(store: DeleteStore, repository: BaseRepository, session: AsyncSession):
-    filters = store.model_dump()
-    result = await repository.delete(Store, session, **filters)
-    if not result:
-        return error_response("Can't delete a store with this ID.")
-
-    return result
+async def get_store_service():
+    return store_service
