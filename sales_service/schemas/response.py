4c6bcf5ce6e3d472af97a3575a4cfd09bb2d3837
evtepo <andrey_laguta00@mail.ru>
2024-10-26 23:46:49 +1000

    дополнить схемы ответов

--- sales_service/schemas/response.py
+++ sales_service/schemas/response.py
@@ -1,26 +1,33 @@
 from datetime import datetime
+from uuid import UUID
 
-from schemas.city import IdCityMixin, NameCityMixin
+from schemas.city import CityIdMixin, NameCityMixin
 from schemas.product import ProductIdMixin, ProductMixin
+from schemas.sales import SaleIdMixin, SaleMixin
 from schemas.store import StoreIdMixin, StoreMixin
 
 
-class CityResponse(IdCityMixin, NameCityMixin):
+class BaseResponse:
     class Config:
         from_attributes = True
 
 
-class StoreResponse(StoreIdMixin, StoreMixin):
-    class Config:
-        from_attributes = True
+class CityResponse(BaseResponse, CityIdMixin, NameCityMixin): ...
 
 
-class ProductResponse(ProductIdMixin, ProductMixin):
+class StoreResponse(BaseResponse, StoreIdMixin, StoreMixin): ...
+
+
+class ProductResponse(BaseResponse, ProductIdMixin, ProductMixin):
+    sales_id: UUID | None
     created_at: datetime
     updated_at: datetime
 
-    class Config:
-        from_attributes = True
+
+class SaleResponse(BaseResponse, SaleIdMixin, SaleMixin):
+    amount: int
+    price: float
+    sale_date: datetime
 
 
 class SingleCityResponse(CityResponse):
@@ -28,10 +35,14 @@ class SingleCityResponse(CityResponse):
 
 
 class SingleProductResponse(ProductResponse):
-    # Добавить Sales
+    sales: SaleResponse | None
     store: StoreResponse
 
 
 class SingleStoreResponse(StoreResponse):
     city: CityResponse
+    products: list[None | ProductResponse]
+
+
+class SingleSaleResponse(SaleResponse):
     products: list[None | ProductResponse]
