b5a522d0fe61ab4c2dc5b979cc1198f96d59224f
evtepo <andrey_laguta00@mail.ru>
2024-10-26 23:43:54 +1000

    убрать модель из аргумента

--- sales_service/api/v1/city.py
+++ sales_service/api/v1/city.py
@@ -2,7 +2,6 @@ from uuid import UUID
 
 from fastapi import APIRouter, Query, status
 
-from models.location import City
 from schemas.response import CityResponse, SingleCityResponse
 from schemas.city import CreateCity, DeleteCity, UpdateCity
 from services.city import (
@@ -20,12 +19,12 @@ router = APIRouter(prefix="/api/v1/city", tags=["City"])
 
 @router.post("/", response_model=CityResponse, status_code=status.HTTP_201_CREATED)
 async def create_city(city: CreateCity, repository: repository_dependency, session: db_dependency):
-    return await new_city(city, City, repository, session)
+    return await new_city(city, repository, session)
 
 
 @router.get("/{city_id}", response_model=SingleCityResponse, status_code=status.HTTP_200_OK)
 async def get_city(city_id: UUID, repository: repository_dependency, session: db_dependency):
-    return await get_single_city(city_id, City, repository, session)
+    return await get_single_city(city_id, repository, session)
 
 
 @router.get(
@@ -39,14 +38,14 @@ async def get_cities(
     page: int = Query(ge=1, default=1),
     size: int = Query(ge=10, le=50, default=10),
 ):
-    return await get_list_of_cities(page, size, City, repository, session)
+    return await get_list_of_cities(page, size, repository, session)
 
 
 @router.put("/", response_model=CityResponse, status_code=status.HTTP_200_OK)
 async def update_city(city: UpdateCity, repository: repository_dependency, session: db_dependency):
-    return await update_city_by_id(city, City, repository, session)
+    return await update_city_by_id(city, repository, session)
 
 
 @router.delete("/", response_model=dict[str, str], status_code=status.HTTP_200_OK)
 async def delete_city(city: DeleteCity, repository: repository_dependency, session: db_dependency):
-    return await delete_city_by_id(city, City, repository, session)
+    return await delete_city_by_id(city, repository, session)
