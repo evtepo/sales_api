1b7726c84988ef35bc318489808fbd41c7ccf56b
evtepo <andrey_laguta00@mail.ru>
2024-10-27 01:10:13 +1000

    изменить логику взаимодействия для product

--- sales_service/api/v1/product.py
+++ sales_service/api/v1/product.py
@@ -1,30 +1,37 @@
+from typing import Annotated
 from uuid import UUID
 
-from fastapi import APIRouter, Query, status
+from fastapi import APIRouter, Depends, Query, status
 
 from schemas.response import ProductResponse, SingleProductResponse
 from schemas.product import CreateProduct, DeleteProduct, UpdateProduct
-from services.product import (
-    delete_product_by_id,
-    get_list_of_products,
-    get_single_product,
-    new_product,
-    update_product_by_id
-)
+from services.product import get_product_service, ProductService
 from utils.dependency import db_dependency, repository_dependency
 
 
 router = APIRouter(prefix="/api/v1/product", tags=["Product"])
 
+product_dependency = Annotated[ProductService, Depends(get_product_service)]
+
 
 @router.post("/", response_model=ProductResponse, status_code=status.HTTP_201_CREATED)
-async def create_product(product: CreateProduct, repository: repository_dependency, session: db_dependency):
-    return await new_product(product, repository, session)
+async def create_product(
+    product: CreateProduct,
+    repository: repository_dependency,
+    session: db_dependency,
+    product_service: product_dependency,
+):
+    return await product_service.new_product(product, repository, session)
 
 
 @router.get("/{product_id}", response_model=SingleProductResponse, status_code=status.HTTP_200_OK)
-async def get_product(product_id: UUID, repository: repository_dependency, session: db_dependency):
-    return await get_single_product(product_id, repository, session)
+async def get_product(
+    product_id: UUID,
+    repository: repository_dependency,
+    session: db_dependency,
+    product_service: product_dependency,
+):
+    return await product_service.get_single_product(product_id, repository, session)
 
 
 @router.get(
@@ -35,17 +42,28 @@ async def get_product(product_id: UUID, repository: repository_dependency, sessi
 async def get_products(
     repository: repository_dependency,
     session: db_dependency,
+    product_service: product_dependency,
     page: int = Query(ge=1, default=1),
     size: int = Query(ge=10, le=50, default=10),
 ):
-    return await get_list_of_products(size, page, repository, session)
+    return await product_service.get_list_of_products(size, page, repository, session)
 
 
 @router.put("/", response_model=ProductResponse, status_code=status.HTTP_200_OK)
-async def update_product(product: UpdateProduct, repository: repository_dependency, session: db_dependency):
-    return await update_product_by_id(product, repository, session)
+async def update_product(
+    product: UpdateProduct,
+    repository: repository_dependency,
+    session: db_dependency,
+    product_service: product_dependency,
+):
+    return await product_service.update_product_by_id(product, repository, session)
 
 
 @router.delete("/", response_model=dict[str, str], status_code=status.HTTP_200_OK)
-async def delete_product(product: DeleteProduct, repository: repository_dependency, session: db_dependency):
-    return await delete_product_by_id(product, repository, session)
+async def delete_product(
+    product: DeleteProduct,
+    repository: repository_dependency,
+    session: db_dependency,
+    product_service: product_dependency,
+):
+    return await product_service.delete_product_by_id(product, repository, session)
--- sales_service/services/product.py
+++ sales_service/services/product.py
@@ -5,54 +5,29 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from models.product import Product
 from repository.repository import BaseRepository
 from schemas.product import CreateProduct, DeleteProduct, UpdateProduct
-from utils.error_handling import error_response
+from services.base import BaseService
 
 
-async def new_product(product: CreateProduct, repository: BaseRepository, session: AsyncSession):
-    data = product.model_dump()
-    return await repository.create(data, Product, session)
+class ProductService(BaseService):
+    async def new_product(self, product: CreateProduct, repository: BaseRepository, session: AsyncSession):
+        return await self.create_new_row(product, Product, repository, session)
+
+    async def get_single_product(self, product_id: UUID, repository: BaseRepository, session: AsyncSession):
+        related_fields = ("sales", "store")
+        return await self.get_single_row(product_id, related_fields, Product, repository, session)
+
+    async def get_list_of_products(self, limit: int, offset: int, repository: BaseRepository, session: AsyncSession):
+        return await self.get_list_rows(offset, limit, Product, repository, session)
+
+    async def update_product_by_id(self, product: UpdateProduct, repository: BaseRepository, session: AsyncSession):
+        return await self.update_row_by_id(product, Product, repository, session)
+
+    async def delete_product_by_id(self, product: DeleteProduct, repository: BaseRepository, session: AsyncSession):
+        return await self.delete_row_by_id(product, Product, repository, session)
 
 
-async def get_single_product(product_id: UUID, repository: BaseRepository, session: AsyncSession):
-    filters = {"id": product_id}
-    related_fields = ("sales", "store")
-    result = await repository.get_single(Product, session, related_fields, **filters)
-    if not result:
-        return error_response("Product with such ID not found.")
-
-    return result
+product_service = ProductService()
 
 
-async def get_list_of_products(limit: int, offset: int, repository: BaseRepository, session: AsyncSession):
-    offset_arg = (offset - 1) * limit
-    result = await repository.get_list(Product, session, limit, offset_arg)
-
-    prev_page = offset - 1 if offset > 1 else None
-    next_page = offset + 1 if len(result) == limit else None
-
-    return {
-        "links": {
-            "prev": prev_page,
-            "next": next_page,
-        },
-        "data": result,
-    }
-
-
-async def update_product_by_id(product: UpdateProduct, repository: BaseRepository, session: AsyncSession):
-    data = product.model_dump()
-    filters = {"id": data.get("id")}
-    result = await repository.update(data, Product, session, **filters)
-    if not result:
-        return error_response("Can't update a product with this ID.")
-
-    return result
-
-
-async def delete_product_by_id(product: DeleteProduct, repository: BaseRepository, session: AsyncSession):
-    filters = product.model_dump()
-    result = await repository.delete(Product, session, **filters)
-    if not result:
-        return error_response("Can't delete a product with this ID.")
-
-    return result
+async def get_product_service():
+    return product_service
