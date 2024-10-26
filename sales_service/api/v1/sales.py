059f9909aecfd50e76fffdc93dfb2dfebf0c5918
evtepo <andrey_laguta00@mail.ru>
2024-10-27 01:10:36 +1000

    перенести зависимость для sales

--- sales_service/api/v1/sales.py
+++ sales_service/api/v1/sales.py
@@ -1,13 +1,17 @@
+from typing import Annotated
 from uuid import UUID
 
-from fastapi import APIRouter, Query, status
+from fastapi import APIRouter, Depends, Query, status
 
+from services.sales import SalesLogic, get_sales_logic
 from schemas.response import SaleResponse, SingleSaleResponse
 from schemas.sales import CreateSale, DeleteSale, UpdateSale
-from utils.dependency import db_dependency, repository_dependency, sale_dependency
+from utils.dependency import db_dependency, repository_dependency
 
 
 router = APIRouter(prefix="/api/v1/sales", tags=["Sales"])
+
+sale_dependency = Annotated[SalesLogic, Depends(get_sales_logic)]
 
 
 @router.post("/", response_model=SaleResponse, status_code=status.HTTP_201_CREATED)
@@ -48,20 +52,17 @@ async def get_sales(
         description="Usage: 5 for >= or -5 for <=",
     ),
 ):
-    try:
-        return await sale_logic.get_list_of_sales(
-            size,
-            page,
-            city,
-            store,
-            product,
-            days,
-            price,
-            amount,
-            session,
-        )
-    except Exception as ex:
-        print(ex)
+    return await sale_logic.get_list_of_sales(
+        size,
+        page,
+        city,
+        store,
+        product,
+        days,
+        price,
+        amount,
+        session,
+    )
 
 
 @router.put("/", response_model=SaleResponse, status_code=status.HTTP_200_OK)
--- sales_service/utils/dependency.py
+++ sales_service/utils/dependency.py
@@ -5,10 +5,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from db.db_connect import get_session
 from repository.repository import BaseRepository, get_repository
-from services.sales import get_sales_logic, SalesLogic
 
 
 db_dependency = Annotated[AsyncSession, Depends(get_session)]
 repository_dependency = Annotated[BaseRepository, Depends(get_repository)]
-
-sale_dependency = Annotated[SalesLogic, Depends(get_sales_logic)]
